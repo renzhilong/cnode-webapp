630eb9c01c3067339588b6d9b753948d110c79ee
renzhilong <fgyls@qq.com>
2017-06-08 12:25:04 +0800

    Initial commit

---
+++ src/page/topic/topic.list.js
@@ -0,0 +1,143 @@
+import React, {Component} from 'react'
+import {Link,hashHistory} from 'react-router'
+import {connect} from 'react-redux'
+import {loadTopics,setNavBarTitle,setNavBarPoints} from 'REDUX/action'
+import {getTopicAndBg,dateDiff} from 'SYSTEM/tool'
+import { ListView,Icon} from 'antd-mobile'
+import {setSessionData,getSessionData} from 'SYSTEM/system';
+class TopicsList extends Component {
+    //备注：全部、精华、招聘、分享、问答共用一个路由，一个组件，所以写了一些很奇怪的方法
+    constructor(props){
+        super(props);
+        let {params:{tabName},topics} = this.props;
+        this.setScrollTop=this.setScrollTop.bind(this)
+    }
+    componentWillMount(){
+        const {topics,params:{tabName}} = this.props;
+        if(topics[tabName].page) return;
+        this._loadMoreData();
+    }
+    //设置滚动条位置
+    setScrollTop(){
+        const {topics:{isLoadingMore},params:{tabName}} = this.props;
+        let topicScroll= getSessionData('topicScroll')||[];
+        let index=topicScroll.findIndex(function(value, index, arr) {
+            return value.tabName == tabName;
+        });
+        let scroll=index==-1?0:topicScroll[index].scroll;
+        if(isLoadingMore){
+            //正在加载延时调用自己
+            window.setTimeout(this.setScrollTop,150)
+        }else {
+            document.body.scrollTop=document.documentElement.scrollTop=scroll
+        }
+
+    }
+    setHeaderTitle(title){
+        const {dispatch,params} = this.props;
+        let topics=getTopicAndBg(title||params.tabName).type;
+        dispatch(setNavBarTitle(topics));
+        dispatch(setNavBarPoints({
+            left:true,
+            right:true,
+            rightContent:()=>{return (<Icon onClick={()=>{hashHistory.push('topics/create')}} type="plus-square" />)},
+            color:'',
+            iconName:'bars'
+        }));
+    }
+    componentDidMount(){
+        this.setHeaderTitle();
+        this.setScrollTop()
+    }
+    //路由参数改变，重置之前版块的滚动条位置
+    componentWillReceiveProps(nextProps){
+        let self=this;
+        if(this.props.params.tabName !== nextProps.params.tabName){
+            this.saveScroll().then(function () {
+                //先全部归零
+                self.setHeaderTitle(nextProps.params.tabName);
+                document.body.scrollTop=document.documentElement.scrollTop=0;
+                window.setTimeout(self.setScrollTop,100)
+            });
+        }
+    }
+    //路由共用问题，路由参数改变不能及时更新组件，所以采取这笨拙的方法
+    shouldComponentUpdate(nextProps){
+        return true
+    }
+    //使用Promise主要是为了组件没卸载的时候能准确的先设置滚动条位置再返回之前版块的滚动条位置
+    saveScroll(){
+        return new Promise((resolve, reject) => {
+            const {params:{tabName}} = this.props;
+            let topicScroll= getSessionData('topicScroll')||[];
+            let obj={};
+            obj.tabName=tabName;
+            obj.scroll=document.body.scrollTop||document.documentElement.scrollTop;
+            let index=topicScroll.findIndex(function(value, index, arr) {
+                return value.tabName == tabName;
+            });
+            if(index !== -1){
+                topicScroll[index]=obj
+            }else {
+                topicScroll.push(obj)
+            }
+            setSessionData("topicScroll",topicScroll);
+            resolve()
+        });
+    }
+    //组件卸载，存储滚动条位置
+    componentWillUnmount(){
+        this.saveScroll();
+    }
+    _renderRow(rowData,SectionId,rowID) {
+        let obj=getTopicAndBg(rowData);
+        return(
+            <div className="list_item" key={rowID}>
+                <Link className="item_title" to={'topic/'+rowData.id}><span className="topics_tab" style={{backgroundColor:obj.bgColor}}>{obj.type}</span>{rowData.title}</Link>
+                <div className="content_wrapper item_avatar">
+                    <Link to={`user/${rowData.author.loginname}`}>
+                        <img className="border_img" src={rowData.author.avatar_url} />
+                    </Link>
+                    <p>{`${rowData.reply_count}/${rowData.visit_count}`}</p>
+                    <p>{dateDiff(rowData.last_reply_at)}</p>
+                </div>
+            </div>
+        )
+    }
+    _loadMoreData() {
+        let {dispatch,params:{tabName},topics} = this.props;
+        dispatch(loadTopics(tabName,++topics[tabName].page));
+    }
+    onEndReached() {
+        let {topics}=this.props;
+        if (topics.isLoadingMore ||topics.isRefreshing) {
+            return;
+        };
+        this._loadMoreData();
+    }
+    render() {
+        let {topics,params:{tabName}}=this.props;
+        const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+        let list=topics[tabName].list;
+        return (
+            <div className="pt_09">
+                <ListView
+                    dataSource={ds.cloneWithRows(list)}
+                    useBodyScroll
+                    renderRow={this._renderRow.bind(this)}
+                    renderSeparator={(sectionID, rowID)=>{return(
+                        <div key={`${sectionID}-${rowID}`} style={{height:'.15rem',backgroundColor:"#f5f5f9"}}></div>
+                    )}}
+                    scrollRenderAheadDistance={900}
+                    onEndReached={this.onEndReached.bind(this)}
+                    initialListSize={list.length-8}
+                    onEndReachedThreshold={10}
+                    scrollEventThrottle={30}
+                />
+            </div>
+        )
+    }
+}
+export default connect(state=>({
+    topics: state.topics
+}))(TopicsList)
---
+++ src/redux/reducers.js
@@ -0,0 +1,175 @@
+import {combineReducers} from 'redux';
+import 'babel-polyfill'
+import {setSessionData,getSessionData,getLocalData} from '../system/system';
+const system = (state = {
+    animating: false,
+}, action)=> {
+    switch (action.type) {
+        case 'set_system_animating':
+            return Object.assign({}, state, {
+                animating: action.animating
+            });
+        default:
+            return state;
+    }
+};
+const navBar=(state={
+    points:{
+        left:true,
+        leftClick:()=>{},
+        rightContent:()=>{},
+        right:false,
+        color:'',
+        iconName:'bars'
+    },
+    title:'全部'
+},action)=>{
+    switch (action.type){
+        case 'set_navBar_points':
+            return Object.assign({},state,{
+                points:action.points
+            });
+        case 'set_navBar_title':
+            return Object.assign({},state,{
+                title:action.title
+            });
+        default:
+            return state;
+    }
+};
+const menu=(state={
+    open:false,
+},action)=>{
+    switch (action.type){
+        case 'set_menu_openState':
+            return Object.assign({},state,{
+                open:action.open
+            });
+        default:
+            return state;
+    }
+};
+const topics=(state={
+    all:{list:[],page:0},
+    good:{list:[],page:0},
+    share:{list:[],page:0},
+    ask:{list:[],page:0},
+    job:{list:[],page:0},
+    detail:{data:{},success:false},
+    isLoadingMore:false
+},action)=>{
+    switch (action.type){
+        case 'set_topics_list':
+            let obj={list: state[action.tabName].list.concat(action.tabData.list),page:action.tabData.page};
+            switch (action.tabName){
+                case 'all':
+                    return Object.assign({}, state, {
+                        all: obj
+                    });
+                case 'good':
+                    return Object.assign({}, state, {
+                        good: obj
+                    });
+                case 'share':
+                    return Object.assign({}, state, {
+                        share: obj
+                    });
+                case 'ask':
+                    return Object.assign({}, state, {
+                        ask: obj
+                    });
+                case 'job':
+                    return Object.assign({}, state, {
+                        job: obj
+                    });
+                default:
+                    return state;
+            }
+        case 'set_topics_loadingMore':
+            return Object.assign({}, state, {
+                isLoadingMore: action.isLoadingMore
+            });
+        case 'set_topics_detail':
+            return Object.assign({}, state, {
+                detail: action.detail
+            });
+        default:
+            return state;
+    }
+};
+const loginModal=(state={
+    isLogin:false,
+    accessToken:getLocalData('accessToken')||'',
+    visible:false,
+},action)=>{
+    switch (action.type){
+        case 'set_loginModal_isLogin':
+            return Object.assign({}, state, {
+                isLogin: action.isLogin
+            });
+        case 'set_loginModal_visible':
+            return Object.assign({}, state, {
+                visible: action.visible
+            });
+        case 'set_loginModal_accessToken':
+            return Object.assign({}, state, {
+                accessToken: action.accessToken
+            });
+        default:
+            return state;
+    }
+};
+const account=(state={
+    info:{
+        avatar_url: "",
+        id: "",
+        loginname: "",
+        success: false
+    },
+    collect:{
+        data:[],
+        success: false
+    },
+    detail:{
+
+    },
+    mesCount:0,
+    messages:{
+        hasnot_read_messages:[],
+        has_read_messages:[]
+    }
+},action)=>{
+    switch (action.type){
+        case 'set_account_info':
+            return Object.assign({}, state, {
+                info: action.info
+            });
+        case 'set_account_collect':
+            return Object.assign({}, state, {
+                collect: action.collect
+            });
+        case 'set_account_detail':
+            return Object.assign({}, state, {
+                detail: action.detail
+            });
+        case 'set_account_mesCount':
+            return Object.assign({}, state, {
+                mesCount: action.mesCount
+            });
+        case 'set_account_messages':
+            return Object.assign({}, state, {
+                messages: action.messages
+            });
+        default:
+            return state;
+    }
+};
+const reducer = combineReducers({
+    navBar,
+    menu,
+    topics,
+    system,
+    loginModal,
+    account
+});
+export  default reducer
